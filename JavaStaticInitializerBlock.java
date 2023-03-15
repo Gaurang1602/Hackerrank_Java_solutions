4e4c1c92302f319cedf90d088e581dc1f0b99531
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-03-15 08:13:53 +0530

    Update JavaStaticInitializerBlock.java

--- JavaStaticInitializerBlock.java
+++ JavaStaticInitializerBlock.java
@@ -36,3 +36,18 @@ java.lang.Exception: Breadth and height must be positive
 
 Solution-
   
+  
+    static int B,H;
+    static Boolean flag=true;
+    static {
+
+   Scanner sc = new Scanner(System.in);
+   B=sc.nextInt();
+   H=sc.nextInt();
+   if(B<=0 || H<=0){
+       flag=false;
+       System.out.println("java.lang.Exception: Breadth and height must be positive");
+   }
+   
+}
+
