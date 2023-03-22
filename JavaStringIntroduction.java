38685c67fd9769be191fba9c93a23a5965b06238
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-03-22 08:31:41 +0530

    Update JavaStringIntroduction.java

--- JavaStringIntroduction.java
+++ JavaStringIntroduction.java
@@ -48,3 +48,24 @@ Solution-
   
   
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    Scanner sc=new Scanner(System.in);
+    String A=sc.next();
+    String B=sc.next();
+    System.out.println((A.length())+(B.length()));
+    char c=A.charAt(0);
+    char d=B.charAt(0);
+    System.out.println((int) c > (int) d?"Yes":"No");
+    System.out.println(A.substring(0, 1).toUpperCase()+A.substring(1)+" "+B.substring(0, 1).toUpperCase()+B.substring(1));
+        
+    }
+}
+
+
+
