69bc86d4ba87bbfcc6cced37c54f337c652f8e19
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-03-30 08:03:56 +0530

    Update JavaStringReverse.java

--- JavaStringReverse.java
+++ JavaStringReverse.java
@@ -16,17 +16,31 @@ Sample Output
 Yes
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   Solution-
   
   
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args)throws IOException {
+        
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s1=br.readLine();
+        String s2=s1;
+        StringBuffer s=new StringBuffer(s1);
+        s.reverse();
+        s1=s.toString();
+        if(s1.compareTo(s2)==0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
