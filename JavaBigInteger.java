a2ee1128a1239e6004fcb02d8a48c24e92a786f3
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-04-11 23:48:51 +0530

    Update JavaBigInteger.java

--- JavaBigInteger.java
+++ JavaBigInteger.java
@@ -30,3 +30,110 @@ Explanation
 
 
 Solution-
+
+  
+  HackerRank Home
+HackerRank
+|
+Prepare
+Certify
+Compete
+Search
+|
+Switch to..
+PrepareJavaBigNumberJava BigInteger
+Java BigInteger
+92 more points to get your gold badge!
+Rank: 273229|Points: 158/250
+Java
+Problem
+Submissions
+Leaderboard
+Discussions
+Editorial
+You made this submission 6 months ago.
+Score:10.00Status:Accepted
+People who solved Java BigInteger attempted this next:
+
+Java BigDecimal
+Learn to handle huge decimals using Java's BigDecimal class.
+Submitted Code
+Language: Java 7
+Open in editor
+1
+import java.io.*;
+2
+import java.util.*;
+3
+import java.text.*;
+4
+import java.math.*;
+5
+import java.util.regex.*;
+6
+​
+7
+public class Solution {
+8
+​
+9
+    public static void main(String[] args) {
+10
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+11
+    
+12
+          Scanner sc = new Scanner(System.in);
+13
+        BigInteger a = new BigInteger(sc.nextLine());
+14
+        BigInteger b = new BigInteger(sc.nextLine());
+15
+         BigInteger sum = a.add(b);
+16
+          BigInteger prod = a.multiply(b);
+17
+        
+18
+        System.out.println(sum+"\n"+prod);
+19
+    }
+20
+}
+21
+        
+22
+    
+23
+​
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+
+Test case 6
+
+Test case 7
+
+Test case 8
+Compiler Message
+Success
+Input (stdin)
+1234
+20
+Expected Output
+1254
+24680
+NEED HELP?
+View discussions
+View editorial
+View top submissions
+BlogScoringEnvironmentFAQAbout UsSupportCareersTerms Of ServicePrivacy Policy
