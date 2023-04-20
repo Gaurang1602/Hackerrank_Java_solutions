aaaf735afd83e4cb4e1a8826598b434e69398a35
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-04-20 21:50:36 +0530

    Update Java_1D_Array.java

--- Java_1D_Array.java
+++ Java_1D_Array.java
@@ -61,3 +61,24 @@ When we save each integer to its corresponding index in , we get . The locked co
   Solution-
   
   
+  import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<a.length;i++){
+            a[i]=scan.nextInt();
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+  
